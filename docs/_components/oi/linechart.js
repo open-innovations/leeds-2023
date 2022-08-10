37d975c221b1215f4243a1c1b485507fdef359bb
Giles Dring <giles@dringtech.com>
2022-08-10 09:29:52 +0100

    Switch oi chart to use auto dependencies

--- docs/_components/oi/linechart.js
+++ docs/_components/oi/linechart.js
@@ -1,13 +1,3 @@
-function initLineChart() {
-  const lineChartReady = new CustomEvent("oi-linechart-ready");
-  if (document.head.querySelector('script[src*="oi.linechart.min.js"]') === null) {
-    const script = document.createElement('script');
-    script.src = 'https://open-innovations.github.io/leeds-2023/js/oi.linechart.min.js';
-    script.addEventListener('load', () => document.dispatchEvent(lineChartReady));
-    document.head.appendChild(script);
-  };
-}
-
 export default () => {
-  return `<script>${ initLineChart.toString() }; initLineChart();</script><div data-dependencies="/js/oi.linechart.js"></div>`;
+  return `<div data-dependencies="/js/oi.linechart.min.js"></div>`;
 }

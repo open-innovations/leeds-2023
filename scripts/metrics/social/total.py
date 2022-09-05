57e85ad2455e065efaad66991b5dc5c4762b5123
Daniel Boam <daniel.boam@open-innovations.org>
2022-09-05 13:00:21 +0100

    Keep Date (index)

--- scripts/metrics/social/total.py
+++ scripts/metrics/social/total.py
@@ -4,4 +4,4 @@ DATA_DIR = "docs\\_data\\metrics\\social_media"
 OUT = "docs\\_data\\metrics\\social_media_2\\all_weekly.csv"
 all = combine.combine_dir(DATA_DIR).groupby(by="week_starting").sum()
 print(all)
-all.to_csv(OUT,index=False)
+all.to_csv(OUT,index=True)

0b271846b50980cfb38392a2e4087922365c14cb
Giles Dring <giles@dringtech.com>
2022-09-20 12:43:11 +0100

    Add reports for LCC period

--- scripts/metrics/media_coverage.py
+++ scripts/metrics/media_coverage.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import datetime
 
 PATH_EXCEL = os.path.join("working", "Leeds 2023 AVE.xlsx")
 OUTPUT_PATH = os.path.join("data", "metrics", "media_coverage")
@@ -57,18 +58,60 @@ def read_media_metrics(sheet_name='Aug 2021 - July 2022'):
     media_data.to_csv(RAW_CSV, index=False)
 
 
-def summarise():
-    data = pd.read_csv(RAW_CSV, usecols=['Date', 'Subject', 'CIRC'], parse_dates=["Date"])
+def load_raw_data(cols=None):
+    '''Load raw data from CSV. Optionally specify columns.'''
+    data = pd.read_csv(RAW_CSV, usecols=cols, parse_dates=["Date"])
+    return data
 
-    data['Year'] = data.Date.dt.to_period('A-JUL')
 
+def media_summary(data):
     summary = pd.DataFrame({
-      'count': data.groupby('Year').count()['Subject'],
-      'reach': data.groupby('Year').sum()['CIRC']
+      'count': data.Subject.count(),
+      'cumulative_count': data.Subject.count().cumsum(),
+      'reach': data.CIRC.sum(),
+      'cumulative_reach': data.CIRC.sum().cumsum(),
     })
-    
+    return summary
+
+
+def filter_coverage_by_period(earliest, latest):
+    raw = load_raw_data(cols=['Date', 'Subject', 'CIRC', 'Month'])
+    filtered = raw[ (raw.Date >= earliest) & (raw.Date < latest) ]
+    return filtered
+
+
+def summarise():
+    data = load_raw_data(cols=['Date', 'Subject', 'CIRC'])
+
+    data['Period'] = data.Date.dt.to_period('A-JUL')
+    annual = data.groupby('Period')
+
+    summary = media_summary(annual)
     os.makedirs(SITE_DATA_PATH, exist_ok=True)
     summary.to_csv(os.path.join(SITE_DATA_PATH, 'summary.csv'))
+
+    # Prepare paths for KPI reports
+    KPI_REPORT_PATH = os.path.join(SITE_DATA_PATH, 'kpi')
+    os.makedirs(KPI_REPORT_PATH, exist_ok=True)
+
+    # Prep data for LCC 2021 report
+    lcc_21_22 = media_summary(
+      filter_coverage_by_period(
+        earliest = datetime.datetime.fromisoformat('2021-04-01'),
+        latest = datetime.datetime.fromisoformat('2022-04-01')
+      ).groupby('Month')
+    )
+    lcc_21_22.to_csv(os.path.join(KPI_REPORT_PATH, 'lcc_21_22.csv'))
+
+    # Prep data for LCC 2022-24 report
+    lcc_22_24 = media_summary(
+      filter_coverage_by_period(
+        earliest = datetime.datetime.fromisoformat('2022-04-01'),
+        latest = datetime.datetime.fromisoformat('2024-04-01')
+      ).groupby('Month')
+    )
+    lcc_22_24.to_csv(os.path.join(KPI_REPORT_PATH, 'lcc_22_24.csv'))
+
 
 def main():
     read_media_metrics()

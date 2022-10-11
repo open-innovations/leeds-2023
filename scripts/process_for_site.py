54acefccd7dd89be9319c124d9232ced3293a047
Giles Dring <giles.dring@open-innovations.org>
2022-10-11 11:35:42 +0000

    Removing roadshows from daily processing

--- scripts/process_for_site.py
+++ scripts/process_for_site.py
@@ -1,12 +1,10 @@
 import util.logger
 import metrics.media_coverage
-import scripts.metrics.roadshow_attendees.__main__
 import metrics.volunteers.process
 import metrics.ballot.summarize
 
 
 def main():
-    scripts.metrics.roadshow_attendees.__main__.process_roadshow_attendees('W-Mon')
     metrics.media_coverage.summarise()
     metrics.volunteers.process.summarise()
 

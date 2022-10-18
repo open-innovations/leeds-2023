d3496abcd3ab31047b50dd21cc35dd20e47a4d02
slowe <299787+slowe@users.noreply.github.com>
2022-10-18 16:12:54 +0100

    Add dark magenta
    Uses magenta button hover colour from Leeds 2023 website

--- docs/_data/colours.js
+++ docs/_data/colours.js
@@ -5,12 +5,13 @@ export const yellow = 'hsl(52, 100%, 50%)';
 export const magenta = 'hsl(321, 100%, 50%)';
 export const cyan = 'hsl(173, 65%, 61%)';
 export const darkcyan = 'hsl(191, 57%, 15%)';
+export const darkmagenta = 'rgb(94, 14, 55)';
 
 export const scales = {
 	'mint': ColourScale(mint+' 0%, '+darkcyan+' 100%'),
 	'yellow': ColourScale(yellow+' 0%, '+darkcyan+' 100%'),
 	'cyan': ColourScale(cyan+' 0%, '+darkcyan+' 100%'),
-	'magenta': ColourScale(magenta+' 0%, rgb(0, 0, 0) 100%')
+	'magenta': ColourScale(magenta+' 0%, '+darkmagenta+' 100%')
 };
 
 //console.log(Colour('hsl(87, 57%, 86%)'));

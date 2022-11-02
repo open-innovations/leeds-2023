aa37793368db50272f3d7a8cd67d5bad65ae76dd
Giles Dring <giles.dring@open-innovations.org>
2022-11-02 19:58:00 +0000

    Update community dashboard

--- scripts/dashboard/community/residents.py
+++ scripts/dashboard/community/residents.py
@@ -4,13 +4,15 @@ from util.geography import LEEDS_LA_CODE
 
 
 def summarise_activity():
+    name_map = pd.read_csv(
+        'data/reference/leeds_wards.csv', index_col='WD21CD')
+
     # Ballot
     ballot = pd.read_csv('data/metrics/ballot/ballot_entries.csv', usecols=[
         'date_submitted', 'ward_code', 'la_code'], parse_dates=['date_submitted']).rename(columns={'date_submitted': 'count'})
     ballot = ballot[ballot['la_code'] == LEEDS_LA_CODE].drop(
         columns=['la_code']).groupby('ward_code').count().reset_index()
     ballot['activity'] = 'ballot'
-    print(ballot)
 
     volunteers = pd.read_csv('data/metrics/volunteers/volunteers.csv', usecols=[
         'hash', 'ward_code', 'local_authority_code']).rename(columns={'hash': 'count'})
@@ -18,17 +20,25 @@ def summarise_activity():
         columns=['local_authority_code']).groupby('ward_code').count().reset_index()
     volunteers['activity'] = 'volunteering'
 
+    roadshow = pd.read_csv('docs/_data/metrics/roadshows/by_ward.csv', usecols=[
+                           'ward_code', 'contact_consents']).rename(columns={'contact_consents': 'count'})
+    roadshow = roadshow[roadshow.ward_code.isin(name_map.index.values)]
+    roadshow['activity'] = 'roadshow_contact'
+
+    # Create report
     report = pd.concat([
         ballot,
         volunteers,
+        roadshow,
     ]).pivot(columns='activity', index='ward_code').droplevel([0], axis='columns').fillna(0).astype(int)
 
+    # Add a total column
     report['total'] = report.sum(1)
 
-    name_map = pd.read_csv(
-        'data/reference/leeds_wards.csv', index_col='WD21CD')
+    # Augment the table with the ward name
     report = report.merge(name_map, left_index=True, right_index=True).rename(
         columns={'WD21NM': 'ward_name'}).sort_values(by=['ward_name'])
     report.index.names = ['ward_code']
 
+    # Save to CSV
     report.to_csv('docs/dashboard/community/_data/residents.csv')

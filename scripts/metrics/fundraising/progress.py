154915cac26b73fd285f5d7d114084272cea1077
Daniel Boam <daniel.boam@open-innovations.org>
2022-11-07 10:32:41 +0000

    Group fundraising by type and stage

--- scripts/metrics/fundraising/progress.py
+++ scripts/metrics/fundraising/progress.py
@@ -5,18 +5,30 @@ def process():
     data = pd.read_excel('working/LEEDS 2023 Fundraising Progress.xlsx',
       sheet_name='Progress', skiprows=3,
       usecols=[
+        'Date Applied',
         'Organisation/Individual', 'Project',
         'Funding type', 'Restricted/ Unrestricted',
         'Application status',
         'Amount '
       ])
+    data = data.rename(columns={'Date Applied' : 'type', 'Application status' : 'status' })
+    data['type'] = data['type'].replace('Oct 2021','TRUSTS AND FOUNDATIONS').fillna(method='ffill')
     data = data[data['Amount '].notna()]
-    data['Application status'] = data['Application status'].fillna('UNKNOWN').str.upper().str.strip()
+    data['status'] = data['status'].fillna('UNKNOWN').str.upper().str.strip()
     data['Organisation/Individual'] = data['Organisation/Individual'].str.strip()
-    data = data[~data['Application status'].str.match(r'^TOTAL')]
+    data = data[~data['status'].str.match(r'^TOTAL')]
+   
+    data.to_csv('testing.csv',index=False)
+
 
     by_stage = pd.DataFrame({
-      'amount': data.groupby('Application status')['Amount '].sum()
+      'amount': data.groupby('status')['Amount '].sum()
     })
-    by_stage.index.name='status'
     by_stage.to_csv('data/metrics/fundraising/amount_by_stage.csv')
+
+
+    by_stage_and_type = pd.DataFrame({
+      'amount': data.groupby(['type','status'])['Amount '].sum()
+    })
+    print(by_stage_and_type)
+    by_stage_and_type.to_csv('data/metrics/fundraising/amount_by_stage_and_type.csv')

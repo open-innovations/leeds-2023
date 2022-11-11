ff0183cce0976f59cdddb241beec6ea743b1eb41
Giles Dring <giles.dring@open-innovations.org>
2022-11-11 14:24:32 +0000

    Add callouts staging stage

---
+++ scripts/metrics/callouts/stage.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+from util.firebase import pull_collection
+
+STAGING_DIR = 'working/metrics/callout/'
+
+os.makedirs(STAGING_DIR, exist_ok=True)
+
+
+def clean_callout(data):
+    data = data.rename(columns={
+      'dateSubmitted': 'date_submitted',
+    })
+    data.date_submitted = pd.DatetimeIndex(
+        data.date_submitted).tz_localize(None).floor('D')
+    return data
+
+
+if __name__ == '__main__':
+    
+    data = pull_collection(
+      collection_name=u'form-builder-submissions/dancersOfLeeds/responses',
+      fields=[
+        'dateSubmitted',
+        'postcode',
+      ])
+    data = clean_callout(data)
+    data.to_csv(STAGING_DIR + 'dancers_of_leeds.csv', index=False)
--- scripts/util/firebase.py
+++ scripts/util/firebase.py
@@ -1,5 +1,6 @@
 import firebase_admin
 from firebase_admin import firestore
+import pandas as pd
 
 
 def get_db():
@@ -7,3 +8,10 @@ def get_db():
 
     db = firestore.client()
     return db
+
+
+def pull_collection(collection_name, fields):
+    db = get_db()
+    ballot_ref = db.collection(collection_name)
+    docs = ballot_ref.select(fields).stream()
+    return pd.DataFrame([doc.to_dict() for doc in docs])

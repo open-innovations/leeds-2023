019e1a4c53c5c64d2ec4f7b4b106a0f4907ac2bb
Giles Dring <giles@dringtech.com>
2022-11-24 22:20:53 +0000

    Bring schools processing under DVC control

--- scripts/metrics/schools/transform.py
+++ scripts/metrics/schools/transform.py
@@ -2,17 +2,23 @@ import pandas as pd
 
 from util.geography import fuzzy_match_ward_name_to_code
 
+
 def load_schools():
     data = pd.read_excel('working/manual/schools/School Engagement Tracker.xlsx',
-      usecols=['Ward','Total engagements','Total number of learners '],
-    )
+                         usecols=['Ward', 'Total engagements',
+                                  'Total number of learners '],
+                         )
     data = data[data.Ward.notna()]
+    return data
+
+
+def transform(data):
     data['Ward'] = data['Ward'].str.strip()
     data = fuzzy_match_ward_name_to_code(data, ward_name_col='Ward')
     data = data.drop(columns=['Ward'])
     data = data.rename(columns={
-      'Total engagements': 'total_engagements',
-      'Total number of learners ': 'total_number_of_learners'
+        'Total engagements': 'total_engagements',
+        'Total number of learners ': 'total_number_of_learners'
     })
 
     data = data.groupby('ward_code').sum().astype(int)
@@ -20,5 +26,12 @@ def load_schools():
 
     return data
 
+
 def save_schools_data(data):
     data.to_csv('data/metrics/schools/schools_engagement.csv', index=False)
+
+
+if __name__ == '__main__':
+    data = load_schools()
+    data = transform(data)
+    save_schools_data(data)

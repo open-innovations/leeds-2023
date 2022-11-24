47de029ec6c7db480a89713f596e4d68655e9132
Giles Dring <giles@dringtech.com>
2022-11-24 21:45:04 +0000

    Tidy up top-level scripts

--- scripts/process_for_site.py
+++ scripts/process_for_site.py
@@ -1,16 +1,11 @@
-import util.logger
 import metrics.media_coverage
 import metrics.volunteers.process
-# import metrics.ballot.summarize
 import dashboard.community
 
 
 def main():
     metrics.media_coverage.summarise()
     metrics.volunteers.process.summarise()
-
-    # metrics.ballot.summarize.all()
-
     dashboard.community.process()
 
 
--- scripts/update_data.py
+++ scripts/update_data.py
@@ -1,7 +1,6 @@
 from util.logger import logging
 
 import metrics.volunteers.process
-# import metrics.ballot.process
 
 
 def main():
@@ -9,12 +8,6 @@ def main():
         metrics.volunteers.process.update()
     except Exception as e:
         logging.error(e)
-
-    # try:
-    #     metrics.ballot.process.update()
-    # except Exception as e:
-    #     logging.error(e)
-
 
 if __name__ == '__main__':
     main()

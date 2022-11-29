6548b9e7d0a080f1c9a48a95e177c2b22dfc5005
Daniel Boam <98815494+dpboam@users.noreply.github.com>
2022-11-29 10:45:40 +0000

    FIx Process Historic Media Coverage

--- scripts/metrics/media_coverage/combine_cision_historic.py
+++ scripts/metrics/media_coverage/combine_cision_historic.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
 
-RAW_DATA_DIR = os.path.join('working','media-cision') 
+RAW_DATA_DIR = os.path.join('working','manual','media') 
 HISTORIC_PATH = os.path.join(RAW_DATA_DIR,'Leeds 2023 AVE.xlsx')
-CISION_PATH = os.path.join(RAW_DATA_DIR,'Cision - Leeds 2023 23 Sept - 17 Oct for OI.csv')
+CISION_PATH = os.path.join(RAW_DATA_DIR,'Cision - Leeds 2023 23 Sept - 17 Oct for OI.csv') 
 OUTPUT_PATH = os.path.join('data','metrics','media_coverage','combined.csv')
 
 historic_columns = ['Outlet Name','News Headline','News Date','Type','Messaging','Tone','Custom Tags','Tier','Medium','Audience Reach']
@@ -29,11 +29,12 @@ historic_data_2['News Date'] = pd.to_datetime(historic_data_2['News Date'],dayfi
 
 cision_data = pd.read_csv(CISION_PATH,parse_dates=['News Date'],dayfirst=True)
 
-                 
+
 # Drop columns and sort by date
 combined_data = (pd.concat([cision_data,historic_data,historic_data_2])
                     .drop(columns=['Tier','Type','Messaging','News Text'])
                     .sort_values(by='News Date')
                 )
 
+combined_data['Audience Reach'] = combined_data['Audience Reach'].astype('string').str.replace(',|\s|\.0|N/A','',regex=True).replace('',None).astype('Int64')
 combined_data.to_csv(OUTPUT_PATH,index=False)

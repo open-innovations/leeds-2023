ad44b71f96ee16b9e6151595e7bc851ca36e104b
Giles Dring <giles@dringtech.com>
2022-12-06 15:00:08 +0000

    Refine the funding counts

--- scripts/metrics/fundraising/prepare.py
+++ scripts/metrics/fundraising/prepare.py
@@ -21,7 +21,7 @@ def summarise(data):
         'stage': data.stage,
         'count': data.id,
     }).groupby(by=['sector', 'stage']).count().reset_index()
-    count_by_sector_and_stage.pivot(index='sector', columns='stage', values='count').reset_index()[
+    count_by_sector_and_stage.pivot(index='sector', columns='stage', values='count').fillna(0).astype(int).reset_index()[
         ['sector', 'Research', 'Prospect', 'Won', 'Lost', 'Unknown']
     ].to_csv(os.path.join(
         SITE_DATA_PATH, 'count_by_sector_and_stage.csv'), index=False)

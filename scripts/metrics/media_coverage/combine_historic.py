2c6ef3ca32923c0f4226030a510cae97e927dd5d
Giles Dring <giles@dringtech.com>
2022-12-06 15:51:08 +0000

    Adding further metrics stages

--- scripts/metrics/media_coverage/combine_historic.py
+++ scripts/metrics/media_coverage/combine_historic.py
@@ -6,72 +6,73 @@ HISTORIC_PATH = os.path.join(RAW_DATA_DIR,'Leeds 2023 AVE.xlsx')
 CISION_PATH = os.path.join(RAW_DATA_DIR,'Cision - Leeds 2023 23 Sept - 17 Oct for OI.csv') 
 OUTPUT_PATH = os.path.join('data','metrics','media_coverage','combined_historic.csv')
 
-historic_columns = ['Outlet Name','News Headline','News Date','Type','Messaging','Tone','Custom Tags','Tier','Medium','Audience Reach']
-historic_data = pd.read_excel(HISTORIC_PATH,usecols='A:J',nrows=335,sheet_name='Aug 2021 - July 2022',names=historic_columns,parse_dates=['News Date'])
-historic_data['News Date'] = historic_data['News Date'].replace(['November','3.22','8.4.22','29.5.22'],['01.11.21','01.03.22','08.04.22','29.05.22'])
-historic_data['News Date'] = pd.to_datetime(historic_data['News Date'],dayfirst=True)
-historic_data['Tone'] = historic_data['Tone'].replace({'\s*Positive\s*' : 'POS','\s*Negative\s*' : 'NEG','\s*Neutral\s*' : 'NEU'},regex=True)
+if __name__ == '__main__':
+    historic_columns = ['Outlet Name','News Headline','News Date','Type','Messaging','Tone','Custom Tags','Tier','Medium','Audience Reach']
+    historic_data = pd.read_excel(HISTORIC_PATH,usecols='A:J',nrows=335,sheet_name='Aug 2021 - July 2022',names=historic_columns,parse_dates=['News Date'])
+    historic_data['News Date'] = historic_data['News Date'].replace(['November','3.22','8.4.22','29.5.22'],['01.11.21','01.03.22','08.04.22','29.05.22'])
+    historic_data['News Date'] = pd.to_datetime(historic_data['News Date'],dayfirst=True)
+    historic_data['Tone'] = historic_data['Tone'].replace({'\s*Positive\s*' : 'POS','\s*Negative\s*' : 'NEG','\s*Neutral\s*' : 'NEU'},regex=True)
 
-historic_columns_2 = ['Tier','Outlet Name','News Date','News Headline','Type','Custom Tags','Medium','Audience Reach']
-historic_data_2 = pd.read_excel(HISTORIC_PATH,usecols='A:H',nrows=217,sheet_name='April 2020-July 2021',names=historic_columns_2,parse_dates=['News Date'])
-historic_data_2['News Date'] = historic_data_2['News Date'].replace(['20.02.02','1/14/1900'],['20.02.20','14.05.21'])
-historic_data_2['Medium'] = historic_data_2['Medium'].str.capitalize()
+    historic_columns_2 = ['Tier','Outlet Name','News Date','News Headline','Type','Custom Tags','Medium','Audience Reach']
+    historic_data_2 = pd.read_excel(HISTORIC_PATH,usecols='A:H',nrows=217,sheet_name='April 2020-July 2021',names=historic_columns_2,parse_dates=['News Date'])
+    historic_data_2['News Date'] = historic_data_2['News Date'].replace(['20.02.02','1/14/1900'],['20.02.20','14.05.21'])
+    historic_data_2['Medium'] = historic_data_2['Medium'].str.capitalize()
 
-#Fill in missing Dates
-historic_data_2.at[54,'News Date'] = '03.03.21'
-historic_data_2.at[55,'News Date'] = '03.03.21'
-historic_data_2.at[95,'News Date'] = '25.05.21'
-historic_data_2.at[142,'News Date'] = '07.08.21'
-historic_data_2.at[143,'News Date'] = '07.08.21'
+    #Fill in missing Dates
+    historic_data_2.at[54,'News Date'] = '03.03.21'
+    historic_data_2.at[55,'News Date'] = '03.03.21'
+    historic_data_2.at[95,'News Date'] = '25.05.21'
+    historic_data_2.at[142,'News Date'] = '07.08.21'
+    historic_data_2.at[143,'News Date'] = '07.08.21'
 
-historic_data_2['News Date'] = pd.to_datetime(historic_data_2['News Date'],dayfirst=True)
-
-
-#cision_data = pd.read_csv(CISION_PATH,parse_dates=['News Date'],dayfirst=True)
-
-columns_order = ['News Date','News Headline','Outlet Name','Audience Reach','Tone','Medium','Custom Tags']
-
-# Drop columns and sort by date
-combined_data = (pd.concat([historic_data,historic_data_2])
-                    .drop(columns=['Tier','Type','Messaging',])
-                    .sort_values(by=['News Date','News Headline','Medium'])
-                    .reindex(columns=columns_order)
-                )
-
-#change column types 
-combined_data['Audience Reach'] = combined_data['Audience Reach'].astype('string').str.replace(',|\s|\.0|N/A','',regex=True).replace('',None).astype('Int64')
-#combined_data['UV*'] = combined_data['UV*'].astype('Int64')
-
-#change column names
-combined_data.columns = combined_data.columns.str.replace(' ','_').str.replace('*','',regex=False).str.lower()
-
-#strip string columns
-df_obj = combined_data.select_dtypes(['object'])
-combined_data[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
+    historic_data_2['News Date'] = pd.to_datetime(historic_data_2['News Date'],dayfirst=True)
 
 
-#Rename outlets to cision name if found
-combined_data['outlet_name'] = combined_data['outlet_name'].replace({
-  'Yorkshire evening post' : 'Yorkshire Evening Post',
-  'Yorksire Evening Post'  : 'Yorkshire Evening Post',
-  'Yorkshire Evening post' : 'Yorkshire Evening Post',
-  'Yorskhire Evening Post' : 'Yorkshire Evening Post',
-  'Yorkshire Eveninig Post' : 'Yorkshire Evening Post',
-  'yorkshire evening post' : 'Yorkshire Evening Post',
-  'The yorkshire evening post' : 'Yorkshire Evening Post',
-  'Yorkshire Post' : 'Yorkshire Post, The',
-  'Yorkshire post' : 'Yorkshire Post, The',
-  'The Yorkshire Post' : 'Yorkshire Post, The',
-  'Yorkshire post culture' : 'Yorkshire Post, The',
-  'Yorkshire  post' : 'Yorkshire Post, The',
-  'Yorkshirepost' : 'Yorkshire Post, The',
-  'Yorkshire post business' : 'Yorkshire Post, The',
-  'Yorkshire Post - Business' : 'Yorkshire Post, The',
-  'yorkshire post' : 'Yorkshire Post, The',
-  'Yorkshireeveningpost.co.uk':'Yorkshire Evening Post - Yorkshireeveningpost.co.uk',
-  'yorkshireeveningpost.co.uk':'Yorkshire Evening Post - Yorkshireeveningpost.co.uk'
-  
-})
+    #cision_data = pd.read_csv(CISION_PATH,parse_dates=['News Date'],dayfirst=True)
+
+    columns_order = ['News Date','News Headline','Outlet Name','Audience Reach','Tone','Medium','Custom Tags']
+
+    # Drop columns and sort by date
+    combined_data = (pd.concat([historic_data,historic_data_2])
+                        .drop(columns=['Tier','Type','Messaging',])
+                        .sort_values(by=['News Date','News Headline','Medium'])
+                        .reindex(columns=columns_order)
+                    )
+
+    #change column types 
+    combined_data['Audience Reach'] = combined_data['Audience Reach'].astype('string').str.replace(',|\s|\.0|N/A','',regex=True).replace('',None).astype('Int64')
+    #combined_data['UV*'] = combined_data['UV*'].astype('Int64')
+
+    #change column names
+    combined_data.columns = combined_data.columns.str.replace(' ','_').str.replace('*','',regex=False).str.lower()
+
+    #strip string columns
+    df_obj = combined_data.select_dtypes(['object'])
+    combined_data[df_obj.columns] = df_obj.apply(lambda x: x.str.strip())
 
 
-combined_data.to_csv(OUTPUT_PATH,index=False)
+    #Rename outlets to cision name if found
+    combined_data['outlet_name'] = combined_data['outlet_name'].replace({
+      'Yorkshire evening post' : 'Yorkshire Evening Post',
+      'Yorksire Evening Post'  : 'Yorkshire Evening Post',
+      'Yorkshire Evening post' : 'Yorkshire Evening Post',
+      'Yorskhire Evening Post' : 'Yorkshire Evening Post',
+      'Yorkshire Eveninig Post' : 'Yorkshire Evening Post',
+      'yorkshire evening post' : 'Yorkshire Evening Post',
+      'The yorkshire evening post' : 'Yorkshire Evening Post',
+      'Yorkshire Post' : 'Yorkshire Post, The',
+      'Yorkshire post' : 'Yorkshire Post, The',
+      'The Yorkshire Post' : 'Yorkshire Post, The',
+      'Yorkshire post culture' : 'Yorkshire Post, The',
+      'Yorkshire  post' : 'Yorkshire Post, The',
+      'Yorkshirepost' : 'Yorkshire Post, The',
+      'Yorkshire post business' : 'Yorkshire Post, The',
+      'Yorkshire Post - Business' : 'Yorkshire Post, The',
+      'yorkshire post' : 'Yorkshire Post, The',
+      'Yorkshireeveningpost.co.uk':'Yorkshire Evening Post - Yorkshireeveningpost.co.uk',
+      'yorkshireeveningpost.co.uk':'Yorkshire Evening Post - Yorkshireeveningpost.co.uk'
+      
+    })
+
+
+    combined_data.to_csv(OUTPUT_PATH,index=False)
--- scripts/metrics/media_coverage/process.py
+++ scripts/metrics/media_coverage/process.py
@@ -6,41 +6,42 @@ DATA_DIR = os.path.join('data','metrics','media_coverage')
 VIEW_DIR = os.path.join('docs','_data','metrics','media_coverage','summary_new')
 
 
-cision_data = pd.read_csv(os.path.join(DATA_DIR,'combined_cision.csv'),parse_dates=['news_date'])
-historic_data = pd.read_csv(os.path.join(DATA_DIR,'combined_historic.csv'),parse_dates=['news_date'])
+if __name__ == '__main__':
+    cision_data = pd.read_csv(os.path.join(DATA_DIR,'combined_cision.csv'),parse_dates=['news_date'])
+    historic_data = pd.read_csv(os.path.join(DATA_DIR,'combined_historic.csv'),parse_dates=['news_date'])
 
-data = pd.concat([cision_data,historic_data]).sort_values('news_date')
+    data = pd.concat([cision_data,historic_data]).sort_values('news_date')
 
-data = data[data['news_date'].between('2021-01-01','2022-12-31')]
+    data = data[data['news_date'].between('2021-01-01','2022-12-31')]
 
 
-outlet_count = pd.DataFrame({'count' : data.groupby(['outlet_name'])['news_headline'].count()}).sort_values('count',ascending=False).head(5)
-outlet_count.to_csv(os.path.join(VIEW_DIR,'outlet_count.csv'))
+    outlet_count = pd.DataFrame({'count' : data.groupby(['outlet_name'])['news_headline'].count()}).sort_values('count',ascending=False).head(5)
+    outlet_count.to_csv(os.path.join(VIEW_DIR,'outlet_count.csv'))
 
-data['medium'] = data['medium'].replace({'Newspaper' : 'Print', 'Magazine' : 'Print','Broadcat' : 'Broadcast', 'Pdf' : 'Online'})
-medium_count = pd.DataFrame({'count' : data.groupby(['medium'])['news_headline'].count()}).sort_values('count',ascending=False)
-medium_count.to_csv(os.path.join(VIEW_DIR,'medium_count.csv'))
+    data['medium'] = data['medium'].replace({'Newspaper' : 'Print', 'Magazine' : 'Print','Broadcat' : 'Broadcast', 'Pdf' : 'Online'})
+    medium_count = pd.DataFrame({'count' : data.groupby(['medium'])['news_headline'].count()}).sort_values('count',ascending=False)
+    medium_count.to_csv(os.path.join(VIEW_DIR,'medium_count.csv'))
 
-data['month'] = data['news_date'].dt.to_period('M')
-month_count = pd.DataFrame({'count' : data.groupby(['month'])['news_headline'].count()})
-month_count.to_csv(os.path.join(VIEW_DIR,'monthly_count.csv'))
+    data['month'] = data['news_date'].dt.to_period('M')
+    month_count = pd.DataFrame({'count' : data.groupby(['month'])['news_headline'].count()})
+    month_count.to_csv(os.path.join(VIEW_DIR,'monthly_count.csv'))
 
-data['custom_tags'] = data['custom_tags'].str.title()
-region_tags = ['International','National','Regional','Local']
-regional_count = pd.DataFrame({'count' : data[data['custom_tags'].isin(region_tags)].groupby(['custom_tags'])['news_headline'].count()}).sort_values('count')
-regional_count.to_csv(os.path.join(VIEW_DIR,'regional_count.csv'))
+    data['custom_tags'] = data['custom_tags'].str.title()
+    region_tags = ['International','National','Regional','Local']
+    regional_count = pd.DataFrame({'count' : data[data['custom_tags'].isin(region_tags)].groupby(['custom_tags'])['news_headline'].count()}).sort_values('count')
+    regional_count.to_csv(os.path.join(VIEW_DIR,'regional_count.csv'))
 
-uv_max = int(data['uv'].max())
-reach_max = int(data['audience_reach'].max())
-stats = ({
-    'total_media' : int(data['news_headline'].count()),
-    'uv_max' : uv_max,
-    'uv_max_outlet' : data[data['uv'] == uv_max]['outlet_name'].values[0],
-    'reach_max' : reach_max,
-    'reach_max_outlet' : data[data['audience_reach'] == reach_max]['outlet_name'].values[0],
-    'total_media_international' : int(data[data['custom_tags'] == 'International']['news_headline'].count())})
+    uv_max = int(data['uv'].max())
+    reach_max = int(data['audience_reach'].max())
+    stats = ({
+        'total_media' : int(data['news_headline'].count()),
+        'uv_max' : uv_max,
+        'uv_max_outlet' : data[data['uv'] == uv_max]['outlet_name'].values[0],
+        'reach_max' : reach_max,
+        'reach_max_outlet' : data[data['audience_reach'] == reach_max]['outlet_name'].values[0],
+        'total_media_international' : int(data[data['custom_tags'] == 'International']['news_headline'].count())})
 
-with open(os.path.join(VIEW_DIR,'stats.yml'),'w') as f:
-    yaml.safe_dump(stats,f)
+    with open(os.path.join(VIEW_DIR,'stats.yml'),'w') as f:
+        yaml.safe_dump(stats,f)
 
 

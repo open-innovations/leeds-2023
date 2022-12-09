5a0470a2ac3fff70e3311a88d4baab3e6517e8f0
Daniel Boam <98815494+dpboam@users.noreply.github.com>
2022-12-09 23:45:38 +0000

    Refactor script

--- scripts/arts_council/process_grants_all.py
+++ scripts/arts_council/process_grants_all.py
@@ -10,10 +10,16 @@ SHEET_NAME = 'Project Grants Awards'
 def get_summarys_all():
     summarys = []
     for year in YEARS:
-        data = pd.read_excel(os.path.join(WORKING_DIR,RAW_FILE_TEMP.format(year)),sheet_name=SHEET_NAME,skiprows=2)
+        data = pd.read_excel(os.path.join(WORKING_DIR,RAW_FILE_TEMP.format(year)),sheet_name=SHEET_NAME,skiprows=2,usecols=['Local authority','Award amount'])
         
-        #replace northamptonshire la values
-        data['Local authority'] = data['Local authority'].replace({
+        summarys.append(data)
+    return pd.concat(summarys)
+
+    
+data = get_summarys_all()
+
+#replace northamptonshire la values
+data['Local authority'] = data['Local authority'].replace({
             'Corby' : 'North Northamptonshire',
             'East Northamptonshire' : 'North Northamptonshire',
             'Kettering' : 'North Northamptonshire' ,
@@ -23,29 +29,10 @@ def get_summarys_all():
             'Daventry' : 'West Northamptonshire'
         })
 
-        summary = pd.DataFrame({
-            'count' : data.groupby('Local authority')['Award amount'].count(),
-            'sum'   : data.groupby('Local authority')['Award amount'].sum()
-        })
-
-        summary.index.name = 'Local Authority'
-
-        summary['year'] = year[:7]
-        summarys.append(summary)
-    return summarys
-
-    
-data = pd.concat(get_summarys_all())
-data=  data.pivot(columns='year')
-
-#clean and add total columns
-data.columns = ['_'.join(col).strip() for col in data.columns.values]
-data['count_total'] = sum([data[col].fillna(0) for col in data.columns if 'count' in col]).astype('Int64')
-data['sum_total'] = sum([data[col].fillna(0) for col in data.columns if 'sum' in col]).astype('Int64')
-data['las'] = data.index
-
-numeric_cols = ['count_2018-19','count_2019-20','count_2020-21','count_2021-22','count_2022-23','sum_2018-19','sum_2019-20','sum_2020-21','sum_2021-22','sum_2022-23']
-data = data.drop(columns=numeric_cols)
+by_la = pd.DataFrame({
+    'count_total' : data.groupby('Local authority')['Award amount'].count().astype('Int64'),
+    'sum_total' : data.groupby('Local authority')['Award amount'].sum().astype('Int64')
+})
 
 #Merge to get local authority code
 with open(os.path.join('working','arts_council','la.json')) as f:
@@ -53,22 +40,20 @@ with open(os.path.join('working','arts_council','la.json')) as f:
 
 hex_data['la_code'] = hex_data.index
 
-combined = pd.merge(hex_data,data,left_on='n',right_on='Local Authority',how='left')
-combined = combined.rename(columns={'n':'local_authority'}).drop(columns=['q','r','region','lad19nmw','colour'])
+combined = (pd.merge(hex_data,by_la,left_on='n',right_index=True,how='left')
+              .rename(columns={'n':'local_authority'}).drop(columns=['q','r','region','lad19nmw','colour']))
 
 
 #Merge with population stats
-
 PS_PATH = os.path.join('working','arts_council','ukpopestimatesmid2020on2021geography.xlsx')
 population_stats = pd.read_excel(PS_PATH,skiprows=7,usecols=['Code','Name','Geography','All ages'])
-population_stats = population_stats[population_stats['Geography'].isin(['Unitary Authority','Metropolitan District','Non-metropolitan District','London Borough','Council Area','Local Government District'])]
 
-combined = (pd.merge(combined,population_stats,left_on='la_code',right_on='Code',how='left')
-               .drop(columns=['las','Name','Code','Geography'])
+
+combined = (pd.merge(combined,population_stats,left_on='la_code',right_on='Code',how='inner')
+               .drop(columns=['Name','Code','Geography'])
                .rename(columns={'All ages' : 'population'})
                .sort_values('la_code'))
 
 combined['sum_total_per_capita'] = ((combined['sum_total'] / combined['population']).round(2)).fillna(0).map('{:,.2f}'.format).replace({'0.00':'0'})
 combined = combined[['la_code','local_authority','population','count_total','sum_total','sum_total_per_capita']]
 combined.to_csv(os.path.join('docs','_data','arts_council','all_summary_hex.csv'),index=False)
-#print(combined)

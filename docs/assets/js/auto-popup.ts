61a32815cb6689cbf79cef310f24540f8f86f246
Giles Dring <giles.dring@open-innovations.org>
2023-03-03 11:30:30 +0000

    Fix linting issues in auto-popup

--- docs/assets/js/auto-popup.ts
+++ docs/assets/js/auto-popup.ts
@@ -1,23 +1,23 @@
 const POPUP_TIMEOUT = 2500;
 const MAX_TIMEOUT = 500;
-addEventListener('DOMContentLoaded', () => {
+addEventListener("DOMContentLoaded", () => {
   // Create and attach the popup
-  const popup = document.createElement('aside');
-  popup.className = 'popup';
-  popup.style.display = 'none';
-  popup.style.opacity = 0;
+  const popup = document.createElement("aside");
+  popup.className = "popup";
+  popup.style.display = "none";
+  popup.style.opacity = "0";
   document.body.appendChild(popup);
 
   // Create variables to hold the target and fader
-  let target = undefined;
+  let target: HTMLElement | undefined = undefined;
   let fader: number | undefined = undefined;
 
   /**
    * Event handler to show the popup.
-   * 
+   *
    * @param event event target
    */
-  function showPopup(event) {
+  function showPopup(event: Event) {
     // Clear any fades
     if (fader) clearTimeout(fader);
 
@@ -25,32 +25,32 @@ addEventListener('DOMContentLoaded', () => {
     if (target === event.currentTarget) return;
 
     // Show the popup
-    popup.style.display = null;
+    popup.style.display = "unset";
 
     // Capture target triggering the popup
-    target = event.currentTarget;
+    target = event.currentTarget as HTMLElement;
+    if (target === undefined) return;
 
     // Get the location of the target
     const loc = target.getBoundingClientRect();
 
     // Get the hover position
-    const autoPopupPos = target.dataset.autoPopupPos || 'bottom';
-    console.log(autoPopupPos);
+    const autoPopupPos = target.dataset.autoPopupPos || "bottom";
 
     // Setup the content and attributes of the popup
-    popup.innerHTML = target.dataset.autoPopup;
+    popup.innerHTML = target.dataset.autoPopup || "";
     popup.dataset.autoPopupPos = autoPopupPos;
 
     // Set the position of the popup based on requested popup position
     switch (autoPopupPos) {
-      case 'right':
-        popup.style.top = loc.top + loc.height / 2 + 'px';
-        popup.style.left = loc.right + 10 + 'px';
+      case "right":
+        popup.style.top = loc.top + loc.height / 2 + "px";
+        popup.style.left = loc.right + 10 + "px";
         break;
-      case 'bottom':
+      case "bottom":
       default:
-        popup.style.top = loc.bottom + 10 + 'px';
-        popup.style.left = loc.left + loc.width / 2 + 'px';
+        popup.style.top = loc.bottom + 10 + "px";
+        popup.style.left = loc.left + loc.width / 2 + "px";
         break;
     }
 
@@ -58,41 +58,41 @@ addEventListener('DOMContentLoaded', () => {
     // This enables any change in visibility to take place before animating opacity
     setTimeout(function () {
       // Set the popup opacity to visible
-      popup.style.opacity = 1;
+      popup.style.opacity = "1";
     });
   }
   /**
    * Factory to create an event listener which hides the popup.
-   * 
+   *
    * @param delay - time period to wait before fading
-   * @returns 
+   * @returns
    */
   function hidePopup(delay = POPUP_TIMEOUT) {
-    return function() {
+    return function () {
       // Create a fader which hides the popup, after a delay
       fader = setTimeout(function () {
         // Fade the opacity
-        popup.style.opacity = 0;
+        popup.style.opacity = "0";
         // Set a timeout to reset everything else
         setTimeout(function () {
-          popup.style.display = 'none';
+          popup.style.display = "none";
           fader = undefined;
           target = undefined;
         }, MAX_TIMEOUT);
       }, delay);
-    }
+    };
   }
 
   // Attach listeners
-  addEventListener('scroll', hidePopup(0));
-  addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') hidePopup(0)();
-  })
+  addEventListener("scroll", hidePopup(0));
+  addEventListener("keydown", (e) => {
+    if (e.key === "Escape") hidePopup(0)();
+  });
   // Get all the hoverable elements
-  const hoverables = document.querySelectorAll('[data-auto-popup]');
-  for (const hoverable of hoverables) {
-    hoverable.addEventListener('mouseover', showPopup);
-    hoverable.addEventListener('focus', showPopup);
-    hoverable.addEventListener('mouseout', hidePopup());
-  }
+  const hoverables = document.querySelectorAll("[data-auto-popup]");
+  hoverables.forEach((hoverable) => {
+    hoverable.addEventListener("mouseover", showPopup);
+    hoverable.addEventListener("focus", showPopup);
+    hoverable.addEventListener("mouseout", hidePopup());
+  });
 });

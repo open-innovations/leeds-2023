6489a48b8c180d7f2f885111ee20d4ce005f0b00
Giles Dring <giles.dring@open-innovations.org>
2023-04-04 22:49:05 +0100

    Pulling schools data from view
    Closes #101

--- scripts/metrics/schools/extract.py
+++ scripts/metrics/schools/extract.py
@@ -5,9 +5,6 @@ import pandas as pd
 
 API_KEY = os.environ['AIRTABLE_API_KEY']
 
-# FIND('CLE - Creative Learning session', {Event+type})
-filter = f.FIND(f.to_airtable_value("CLE - Creative Learning session"), "ARRAYJOIN({Event type})")
-
 WORKING_DIR = os.path.join('working', 'metrics', 'airtable')
 os.makedirs(WORKING_DIR, exist_ok=True)
 
@@ -16,39 +13,14 @@ SCHOOLS_DATA = os.path.join(WORKING_DIR, 'schools_events.csv')
 def fetch_data():
     BASE_ID = 'appHAh7IYG6p2w5Yo'
     TABLE_NAME = 'EVENTS'
+    VIEW_NAME = 'OI Creative Learning Evaluation Data'
 
     table = Table(API_KEY, BASE_ID, TABLE_NAME)
 
-    school_events = table.all(
-        fields=[
-            'Event Unique Identifier',
-            'Event name',
-            'Project name',
-            'Workstream',
-            'Event type',
-            'Season',
-            'Start date',
-            'End date',
-            'Postcode (from Venue)',
-            'Ward (from Venue)',
-            'ACTUAL Audience size / number of participants - IN PERSON',
-            'CLE - Setting',
-            'CLE - Key Stage',
-            'CLE - Subject Area',
-            'CLE - Activity Type',
-            'CLE - School Timing',
-            'School Name (from School)',
-            'School Address Postcode',
-            'Ward (from School)',
-            'Number of booked participants',
-        ],
-        # FIND('CLE - Creative Learning session', {Event+type})
-        formula=filter
-    )
+    school_events = table.all(view=VIEW_NAME)
     return pd.json_normalize([x['fields'] for x in school_events])
 
 
 if __name__ == '__main__':
     data = fetch_data()
-    print(SCHOOLS_DATA)
     data.to_csv(SCHOOLS_DATA, index=False)
--- scripts/metrics/schools/transform.py
+++ scripts/metrics/schools/transform.py
@@ -22,6 +22,112 @@ def literal_converter(series):
 
 
 def read_raw_data():
+    '''
+    These are the available fields in the extracted view:
+
+    Project name
+    Event name
+    Workstream
+    Event type
+    L23 Creative Team Lead
+    Season
+    Access Offer
+    WeTrack ID
+    AirTable ID
+    Is this activity part of a Signature project or a series of different activities under the same banner?
+    Which ticket booking system is used?
+    CLE - Setting
+    CLE - Key Stage
+    CLE - Subject Area
+    CLE - Activity Type
+    CLE - School Timing
+    L23 Production Lead
+    L23 VE Lead
+    Event Unique Identifier
+    Last Modified
+    Post-event evaluation due?
+    Divider1
+    Divider2
+    Divider3
+    Divider4
+    Last Modified By.id
+    Last Modified By.email
+    Last Modified By.name
+    Start date
+    End date
+    Venue (dropdown link)
+    What is the activity category?
+    What time of the day was the activity?
+    How do audiences / participants sign up to attend the activity?
+    ACTUAL Audience size / number of participants - IN PERSON
+    Did you collect any postcode data from audiences / participants?
+    Please provide any other narrative information relating to the event / activity
+    School
+    Does this event need to go on online What's On?
+    Who the transport is for
+    Toilets
+    Food and beverage offer
+    Merchandise offer
+    Dietary requirements catered for
+    Prohibited items list
+    Seating / Standing
+    Ward (from Venue)
+    Venue Name (from Venue)
+    Address Line 1 (from Venue)
+    Address Line 2 (from Venue)
+    Address Line 3 (from Venue)
+    Postcode (from Venue)
+    Ward (from School)
+    School Name (from School)
+    School Address Postcode
+    Format
+    Website / platform for digital presentation
+    ACTUAL Audience size / number of participants - ONLINE
+    Creative Learning activity category
+    What is the activity artform or type of culture?
+    Who will be attending/involved?
+    If 'participants' are involved had they had input before the day of the activity/event?
+    Event Description
+    Status
+    Target audience numbers
+    Single Event or multiple?
+    'Other' activity category - Please give details
+    Using L23 Ticketing Team?
+    "Please provide a narrative description of audiences / participants. This might include: gender, age, ethnicity, disability"
+    "If it is repeated, how many times?"
+    Notes on Date(s) and Time(s)
+    Date Locked
+    Did you collect any demographic data from audiences / participants?
+    Participation Type
+    Age advisory
+    In S2 Guide?
+    Online launch for 30th March
+    Event length
+    Venue notes
+    Written Credits
+    Primary Venue Contact
+    CLE Facilitator
+    Duty Safeguarding Lead
+    Number of booked participants
+    Email (from CLE Facilitator)
+    Name (from CLE Facilitator)
+    Telephone (from CLE Other staff in attendance)
+    CLE Facilitator email
+    CLE Facilitator Phone
+    School Address Line 1
+    School Address Line 2
+    School Address Line 3
+    'Other' artform or type of culture category - Please give details
+    Allocated or Unallocated
+    Ticket pricing policy
+    Start Time
+    End Time
+    Ticketing notes
+    On sale date
+    Price A
+    Total tickets available
+    Target audience profile
+    '''
     # Load the file
     data = pd.read_csv(RAW_SCHOOLS_DATA).apply(func=literal_converter)
 

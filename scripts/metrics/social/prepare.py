7ef417242ea237948987087568007f186f9bd745
Giles Dring <giles.dring@open-innovations.org>
2023-04-26 10:52:20 +0100

    chore: move social media data to metrics dir

--- scripts/metrics/social/prepare.py
+++ scripts/metrics/social/prepare.py
@@ -5,7 +5,7 @@ import os
 TOTAL_PATH_CONFIG = os.path.join('scripts','metrics','social','total_summary_config.yml')
 WEEKLY_PATH_CONFIG = os.path.join('scripts','metrics','social','weekly_summary_config.yml')
 
-TOTAL_PATH_OUT = os.path.join('docs','_data','metrics','social_media','summary.yml')
+TOTAL_PATH_OUT = os.path.join('docs','metrics','social-media','_data','headlines.yml')
 WEEKLY_PATH_OUT = os.path.join('working','junk.yml')
 
 
--- scripts/metrics/social/total.py
+++ scripts/metrics/social/total.py
@@ -2,8 +2,10 @@ import combine
 import yaml
 import os
 
-SUMMARY_DIR = os.path.join('docs','_data','metrics','social_media','summary.yml')
-DATA_DIR = os.path.join('docs','_data','metrics','social_media','service')
+DATA_PATH=os.path.join('docs','metrics','social-media','_data')
+
+SUMMARY_DIR = os.path.join(DATA_PATH,'headlines.yml')
+DATA_DIR = os.path.join(DATA_PATH,'service')
 os.makedirs(DATA_DIR, exist_ok=True)
 
 def summary_total():
@@ -20,7 +22,7 @@ def summary_total():
         yaml.safe_dump(stats,f)
 
 def main():
-    OUT = os.path.join('docs','_data','metrics','social_media','all_weekly.csv')
+    OUT = os.path.join(DATA_PATH,'all_weekly.csv')
     all = combine.combine_dir(DATA_DIR).groupby(by="week_ending").sum()
     all.to_csv(OUT,index=True)
     summary_total()

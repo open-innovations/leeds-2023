4ad8c5efd031a52d9bb840c6950fba4c23128ebd
Giles Dring <giles.dring@open-innovations.org>
2023-05-10 00:13:55 +0100

    Changes to gitPathUdpated filter
    Plus formatting

--- local-lib/filters.ts
+++ local-lib/filters.ts
@@ -1,19 +1,27 @@
-export function lookup(data: Record<string, unknown>[], key: string, entry: string, value: string) {
-  const result = data.find(x => x[key] = entry);
-  if (result === undefined) throw new ReferenceError('No entry found');
+export function lookup(
+  data: Record<string, unknown>[],
+  key: string,
+  entry: string,
+  value: string,
+) {
+  const result = data.find((x) => x[key] = entry);
+  if (result === undefined) throw new ReferenceError("No entry found");
   return result[value];
 }
 
 export function getKey(context: Record<string, unknown>, path: string) {
   return path
-    .split('.')
+    .split(".")
     .reduce(
       (result, key) => result[key] as Record<string, unknown>,
-      context
+      context,
     );
 }
 
 export async function gitPathUpdated(path: string) {
-  const p = await Deno.run({ cmd: ["git", "log", "-1", '--pretty="%cI"', "--", path], stdout: 'piped' })
-  return new TextDecoder().decode(await p.output()).trim().replace(/"/g, '');
+  const p = await Deno.run({
+    cmd: ["git", "log", "-1", '--pretty="%cI"', "--", path],
+    stdout: "piped",
+  });
+  return JSON.parse(new TextDecoder().decode(await p.output()).trim());
 }
